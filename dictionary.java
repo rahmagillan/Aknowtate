f23023dc59a9103ee02339580f150d095b3ec31e
blobbynord <45715642+blobbynord@users.noreply.github.com>
2018-12-08 11:21:28 -0500

    Update dictionary.java

--- dictionary.java
+++ dictionary.java
@@ -1,20 +1,37 @@
 import java.util.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class dictionary {
     
+    
+    
     public static void main(String[] args) throws IOException{
+    	
     	Scanner dict=new Scanner(new File("dictionary.txt"));
-    	String total="";
+    	
+    	HashMap<String, String> map=new HashMap<String, String>();
+    		
     	while (dict.hasNextLine()){
     		String name=dict.nextLine();
-    		total+=" +";
-    		total+=name;
+    		String[] key=name.split("  ");
+    		System.out.println(name);
+    		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    		System.out.println(key[0]);
+    		//map.put(key, name);
+    		try{
+    			dict.nextLine();
+    		}
+    		catch (Exception e){
+    			System.out.println("End of Dictionary");
+    		}
+    		
     	}
-    	
-    	String[] content=total.split("+");
+    	//String[] content=total.split("+");
     	dict.close();
-    	System.out.println(content[0]);
+    	//System.out.println(content[0]);
     	//System.out.println(total);
     	
     }
