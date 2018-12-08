154821089ddec4f45aaa92bb1c5c616983c75a04
blobbynord <45715642+blobbynord@users.noreply.github.com>
2018-12-08 15:31:30 -0500

    Update dictionary.java

--- dictionary.java
+++ dictionary.java
@@ -10,17 +10,55 @@ public class dictionary {
     
     public static void main(String[] args) throws IOException{
     	
-    	Scanner dict=new Scanner(new File("dictionary.txt"));
+    	Scanner dict=new Scanner(new File("dict2.txt"));
+    	
+    	String[] chars={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "|", "[", "]", ";", ":", "'", ",", "<", ".", ">", "/", "?", " "};
+    	String[] spcChars={};
+    	ArrayList<String> str=new ArrayList<String>();
+    	boolean yes=false;
     	
     	HashMap<String, String> map=new HashMap<String, String>();
-    		
-    	while (dict.hasNextLine()){
+	
+		while (dict.hasNextLine()){
+			int n=0;
     		String name=dict.nextLine();
+    		for (int i=0; i<name.length(); i++){
+    			for (String j:chars){
+					if (name.substring(i).equals(j)){
+						System.out.println("");
+						yes=false;
+					}else{
+						String newName=name.substring(0,i)+""+name.substring(i);
+						str.add(newName);
+						yes=true;
+    				}
+    			}
+    		}
+    		
     		String[] key=name.split("  ");
-    		System.out.println(name);
-    		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     		System.out.println(key[0]);
+    		System.out.println(name);
+    		/*if (yes){
+    			System.out.println("si");
+    			System.out.println(str.get(n));
+    		}else{
+    			System.out.println("non");
+    			System.out.println(name);
+    		}*/
+    		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    		
+    		
+    		/*for (int i = 0; i < str.get(0).length(); i++){
+    			if (str.get(0).charAt(i) == 'ï' || str.get(0).charAt(i) == '»' || str.get(0).charAt(i) == '¿') {
+    				System.out.print("");
+    			}
+    			else{
+    				System.out.print(str.get(0).charAt(i));
+    			}
+    		}*/
+    		
     		//map.put(key, name);
+    		n+=1;
     		try{
     			dict.nextLine();
     		}
@@ -28,7 +66,7 @@ public class dictionary {
     			System.out.println("End of Dictionary");
     		}
     		
-    	}
+		}
     	//String[] content=total.split("+");
     	dict.close();
     	//System.out.println(content[0]);
