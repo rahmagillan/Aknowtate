8789462faa3273f5ac5e4299cf0f40bc58983eb8
Rahma Gillan <rahmagillan2001@gmail.com>
2018-12-08 22:16:31 -0500

    Added the beginning of FileIO
    In case I break it I am uploading pls ignore me

--- Main.java
+++ Main.java
@@ -1,21 +1,16 @@
-import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -34,7 +29,7 @@ public class Main extends JFrame implements ActionListener{
 	private MenuPanel menu;
 	
 	public Main() {
-		super("Aknowtate");
+		super("AnKNOWtate");
 		setSize(new Dimension(1200,800));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -65,14 +60,18 @@ public class Main extends JFrame implements ActionListener{
 class MenuPanel extends JPanel implements MouseListener {
 	
 	
-	private boolean reset = false;
+	private File bookpage;
+
 	private Font raleway;
 	private JButton photo = new JButton("Upload Photo");
 	private JButton analyze = new JButton("Analyze");
 	private JButton clear = new JButton("Clear");
 	
-	private JLabel title = new JLabel("Aknowtate");
+	private JLabel title = new JLabel("AnKNOWtate");
 	private JLabel def = new JLabel("Definition: ");
+	private ArrayList<String> text = new ArrayList<String>(); //each element is a line
+	
+	private JLabel page = new JLabel();
 	
 	public MenuPanel() {
 	
@@ -136,6 +135,15 @@ class MenuPanel extends JPanel implements MouseListener {
 		def.setFont(raleway);
 		def.setLocation(new Point(50,90));
 		add(def);
+		
+		
+		raleway = new Font("Raleway",Font.PLAIN,12);
+		page.setVerticalAlignment(SwingConstants.TOP);//the page's label 639, 15, 535, 732
+		
+		page.setSize(new Dimension(535,732));
+		page.setLocation(new Point(639,15));;
+		page.setFont(raleway);
+		add(page);
 	}
 
 	@Override
@@ -144,16 +152,31 @@ class MenuPanel extends JPanel implements MouseListener {
 		if(source == photo) {
 			JFileChooser chooser = new JFileChooser();
 		    FileNameExtensionFilter filter = new FileNameExtensionFilter(
-		        "JPG,PNG,GIF Images", "jpg", "gif","png");
+		        "TXT File", "txt");
 		    chooser.setFileFilter(filter);
 		    int returnVal = chooser.showOpenDialog(getParent());
 		    if(returnVal == JFileChooser.APPROVE_OPTION) {
-		       System.out.println("You chose to open this file: " +
-		            chooser.getSelectedFile().getName());
-		    } 
+		   	   bookpage = new File(chooser.getSelectedFile().getAbsolutePath());
+			
+		   	   if (bookpage != null) {
+			    	Scanner s = null;
+					try {
+						s = new Scanner(bookpage);
+					} catch (FileNotFoundException e1) {}
+			    	while (s.hasNextLine()) {
+			    		text.add(s.nextLine());
+			    	}
+			    	page.setText("");//clear page in case the user did not clear
+			    	for (int i = 0; i < text.size(); i++) {
+			    		//System.out.println(text.get(i));
+			    		
+			    		page.setText(page.getText()+text.get(i)+"\n"); ///////////new linedoes not work
+			    	}
+		   	   }
+		    }
 		}
 		if (source == clear) {
-			reset = true;
+			page.setText("");
 		}
 	}
 	@Override
@@ -214,12 +237,7 @@ class MenuPanel extends JPanel implements MouseListener {
 		g.setColor(Color.BLACK);
 		g.drawRect(639, 15, 535, 732);
 	
-		if (reset) {
-			g.setColor(Color.WHITE);
-			g.fillRect(639, 15, 535, 732);
-			reset = false;
-		}
-	
+			
 	}
 	
 }
